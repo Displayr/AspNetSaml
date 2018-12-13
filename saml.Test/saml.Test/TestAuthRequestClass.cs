4c23c1bf04f1c8bb43db2ea244c6372652ab8c2a
Matthew Ta <matthewt@NUMDOM2.numbers.net.au>
2018-12-13 17:55:06 +1100

    Started working on test cases for AuthRequest

---
+++ saml.Test/saml.Test/TestAuthRequestClass.cs
@@ -0,0 +1,19 @@
+﻿using Saml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Xml;
+
+namespace Saml.Test
+{
+    [TestClass]
+    class TestAuthRequestClass
+    {
+        [TestMethod()]
+        public void TestSomething()
+        {
+
+        }
+    }
+}
---
+++ saml.Test/saml.Test/TestAuthRequestClassSaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Saml.Test
+{
+    [TestClass]
+    public class TestAuthRequestClassSaml
+    {
+        /// <summary> This test ensures that the appropriate exception is thrown when 
+        /// the app ID provided is NULL.
+        /// </summary>
+        [TestMethod]
+        public void TestGetRequestAppIdNull()
+        {
+            AuthRequest auth_request = new AuthRequest(null, null);
+
+            string request = auth_request.GetRequest(AuthRequest.AuthRequestFormat.Base64);
+            Console.WriteLine(request);
+        }
+
+        /// <summary> This test ensures that the appropriate exception is thrown when reply url 
+        /// provided is invalid e.g www.google.casdvvdsfm.
+        /// </summary>
+        [TestMethod]
+        public void TestGetRequestInvalidReplyUrl()
+        {
+            AuthRequest 
+        }
+
+    }
+}
