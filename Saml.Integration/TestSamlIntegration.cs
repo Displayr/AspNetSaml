3eae7370e10f3fed94b1b7cf25163a27a52daaf9
Matthew Ta <matthewt@NUMDOM2.numbers.net.au>
2018-12-19 17:50:51 +1100

    Nearly finished the first integration tset

--- Saml.Integration/TestSamlIntegration.cs
+++ Saml.Integration/TestSamlIntegration.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text;
+using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PuppeteerSharp;
 using System.Threading.Tasks;
 using System.Web;
 using System.Net;
+using static System.Net.Mime.MediaTypeNames;
 
 /// <summary>
 /// Integration testing for the SAML library AspNet.
@@ -24,6 +27,10 @@ namespace Saml.Integration
         const string USERNAME = "intern1@DISPLAYRSAMLTEST.onmicrosoft.com";
         const string PASSWORD = "Testmyapp5";
 
+        const string REPLY_URL = "https://localhost:44376/Home/WelcomeUser";
+        const string APP_ID = "15eedc3e-ead5-47c8-8424-a98027d91da7";
+        const string SAML_ENDPOINT = "https://login.microsoftonline.com/86c4efd3-7f59-4e51-8f64-6d7848dfcaef/saml2";
+
         /// <summary> This test ensures that the in built c# web client is able to use the SAML library to
         /// allow the user to perform single sign on to the azure active directory.
         /// </summary>
@@ -39,15 +46,13 @@ namespace Saml.Integration
             using (var browser = await Puppeteer.LaunchAsync(options))
             using (var page = await browser.NewPageAsync())
             {
-                string saml_endpoint = "https://login.microsoftonline.com/86c4efd3-7f59-4e51-8f64-6d7848dfcaef/saml2";
-
                 AuthRequest auth = new AuthRequest(
-                    "15eedc3e-ead5-47c8-8424-a98027d91da7",         // put your app's "unique ID" here
-                    "https://localhost:44376/Home/WelcomeUser"      // assertion Consumer Url - the redirect URL where the provider will send authenticated users
+                    APP_ID,         // put your app's "unique ID" here
+                    REPLY_URL      // assertion Consumer Url - the redirect URL where the provider will send authenticated users
                 );
 
                 // goto the micrsoft login page
-                string sso_redirect = auth.GetRedirectUrl(saml_endpoint);
+                string sso_redirect = auth.GetRedirectUrl(SAML_ENDPOINT);
                 await page.GoToAsync(sso_redirect);
 
                 Console.WriteLine("Redirecting to sso url: " + sso_redirect);
@@ -77,13 +82,38 @@ namespace Saml.Integration
                 await page.ScreenshotAsync(SCREENHOST_PATH + "4.png");
                 
                 await page.ClickAsync(NOT_STAY_SIGNED_IN_BUTTON_SELECTOR);
-                var response = await page.WaitForNavigationAsync();
+
+                // this is actually a request from the azure IDP
+                var response = await page.WaitForRequestAsync(REPLY_URL);
                 await page.ScreenshotAsync(SCREENHOST_PATH + "5.png");
 
-                string saml_response = response.Request.PostData.ToString();
+                // we can't do much with a NULL response so just leave here.
+                Assert.IsNotNull(response);
 
-                // grab the SAML token provided by the IDP
-               
+                // grab the SAML token provided by the IDP and ensure 
+                // and ensure it contains relevant information
+                string saml_response = response.PostData.ToString();
+                System.Text.UTF8Encoding enc = new System.Text.UTF8Encoding();
+
+                int index = saml_response.IndexOf("SAMLResponse=");
+                int len = "SAMLResponse=".Length;
+
+                string saml_formatted = saml_response.Substring(index + len);
+
+                string xml_contents = enc.GetString(Convert.FromBase64String(saml_formatted));
+
+                XmlDocument xml_doc = new XmlDocument();
+                xml_doc.PreserveWhitespace = true;
+                xml_doc.XmlResolver = null;
+                xml_doc.LoadXml(xml_contents);
+
+                XmlNamespaceManager manager = new XmlNamespaceManager(xml_doc.NameTable);
+                //manager.AddNamespace("ds", SignedXml.XmlDsigNamespaceUrl);
+                manager.AddNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
+                manager.AddNamespace("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
+
+                XmlNode node = xml_doc.SelectSingleNode("/samlp:Response/saml:Assertion/saml:AttributeStatement/saml:Attribute[@Name='http://schemas.xmlsoap.org/ws/2005/05/identity/claims/displayname']/saml:AttributeValue", manager);
+                Console.WriteLine("Display name: \n" + node.InnerText);
 
                 await page.CloseAsync();
                 await browser.CloseAsync();
